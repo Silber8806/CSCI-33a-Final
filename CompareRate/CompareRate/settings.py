5be1c17be851fc37f230ab545f07669c4e7120af
Chris <Silber8806@gmail.com>
2019-07-30 23:36:11 -0400

    updating system

--- CompareRate/CompareRate/settings.py
+++ CompareRate/CompareRate/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'liabilities.apps.LiabilitiesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'liabilities.apps.AccountsConfig',
     'accounts.apps.AccountsConfig',
 ]
 
